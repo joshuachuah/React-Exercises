d61183be7a0ff9fa7174fd2f472a1aed21475308
Joshua <xjoshua96@gmail.com>
2024-04-30 23:38:03 -0400

    useContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './App.css';
-import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
 
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching/>
+      <UserContext.Provider value={'cereal'}>
+        <ChannelContext.Provider value={'Hog Rider'}>
+          <ComponentC/>
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
-  );
+  )
 }
 
 export default App;
