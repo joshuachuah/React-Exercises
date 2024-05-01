df76a1ed67807332978fc3630d44a987ba15ad06
Joshua <xjoshua96@gmail.com>
2024-04-30 23:51:45 -0400

    uesContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import './App.css';
-import ComponentC from './components/ComponentC';
-
-
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+import CounterOne from './components/CounterOne';
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'cereal'}>
-        <ChannelContext.Provider value={'Hog Rider'}>
-          <ComponentC/>
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne/>
     </div>
   )
 }
