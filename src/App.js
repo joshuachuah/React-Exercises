c783206ba5cd5481d0ecf9271c601e7a1010171f
Joshua <xjoshua96@gmail.com>
2024-05-01 14:01:50 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
 import CounterThree from './components/CounterThree';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne/> */}
       {/* <CounterTwo/> */}
-      <CounterThree/>
+      {/* <CounterThree/> */}
+      {/* <DataFetchingOne/> */}
+      <DataFetchingTwo/>
     </div>
   )
 }
